55a14e1c762a19b364321a18efc64a8b3724c1df
Jennifer <lopesdossantosjennifergabriely@gmail.com>
2026-06-18 10:24:59 -0300

    Adiciona mais aulas

---
+++ Aulas/Aula 18 - Trabalhando com arquivos/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Aula_18___Trabalhando_com_arquivos;
+using System.IO;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            File.WriteAllText("file.txt", "Hello World");
+            Console.WriteLine("Arquivo criado com sucesso");
+
+            // Criar o arquivo em um local especifico 
+            string path = @"C:\\Users\\ASUS\\source\\repos\\Aulas\\Aula 18 - Trabalhando com arquivos/fileName.txt";
+            string content = "Hello World";
+            File.WriteAllText(path, content);
+            Console.WriteLine("Arquivo criado com sucesso");
+
+            // Criar um arquivo word
+            string path1 = @"C:\\Users\\ASUS\\source\\repos\\Aulas\\Aula 18 - Trabalhando com arquivos/";
+            string fileName = "fileNameWord.doc";
+            string filePath = path1 + fileName;
+
+            string content1 = "Hello World";
+            File.WriteAllText(filePath, content1);
+            Console.WriteLine("Arquivo criado com sucesso");
+
+            // Inserir um paragrafo em um arquivo 
+            string path2 = @"C:\\Users\\ASUS\\source\\repos\\Aulas\\Aula 18 - Trabalhando com arquivos/";
+            string fileName1 = "fileNameWord.doc";
+            string filePath1 = path2 + fileName1;
+
+            string addContent = "Curso de .NET";
+            File.AppendAllText(filePath1, addContent);
+            Console.WriteLine("Arquivo atualizado com sucesso");
+
+            //Ler o arquivo
+            string fileContent = File.ReadAllText(filePath1);
+            Console.WriteLine("Conteudo do arquivo" + fileContent);
+    }
+}
+
---
+++ Aulas/Aula17 - Listas/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Aula17___Listas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> fruits = new List<string>()
+            {
+                "Mango",
+                "Banana",
+                "Apple",
+                "Grapes"
+            };
+
+            //acessar valores 
+            Console.WriteLine(fruits[0]);
+
+            foreach(string fruit in fruits)
+            {
+                Console.WriteLine(fruit);
+            }
+
+            // adicionar um valor
+            fruits.Add("Pineapple");
+
+            // remover um valor
+            fruits.Remove("Banana");
+
+            // contar quantos itens tem na lista
+            Console.WriteLine(fruits.Count);
+
+            // modificar um elemento 
+            fruits[1] = "Strawberry";
+
+            // limpar a lista
+            fruits.Clear();
+
+            // declarar sem inicializar
+            List<int> numbers = new List<int>();
+
+            numbers.Add(10);
+            numbers.Add(2);
+            numbers.Add(50);
+
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+    }
+}
+}
