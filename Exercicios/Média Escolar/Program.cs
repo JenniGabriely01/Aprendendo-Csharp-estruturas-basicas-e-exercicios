1d045ef4fb717c56136f4ba09fd630484b07e440
Jennifer <lopesdossantosjennifergabriely@gmail.com>
2026-06-18 18:52:06 -0300

    Add exercicio

---
+++ Exercicios/Média Escolar/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Média_Escolar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //  receber 3 notas, calcular a média e mostrar se foi reprovado 
+            Console.WriteLine("Digite a primeira nota: ");
+            double n1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite a segunda nota: ");
+            double n2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite a terceira nota: ");
+            double n3 = Convert.ToDouble(Console.ReadLine());
+
+            double media = (n1 + n2 + n3) / 3;
+            if(media > 6)
+            {
+                Console.WriteLine("Aprovado");
+            } else if (media > 3 && media < 6)
+            {
+                Console.WriteLine("Recuperação");
+            } else
+            {
+                Console.WriteLine("Reprovado");
+            }
+        }
+    }
+}
---
+++ Exercicios/calc/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace calc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("digite o primeiro número: ");
+            double n1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("digite o segundo número: ");
+            double n2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite a operação");
+            string op = Console.ReadLine();
+
+            switch (op)
+            {
+                case "+":
+                    double soma = n1 + n2;
+                    Console.WriteLine("O resultado é " + soma);
+                    break;
+                case "-":
+                    double sub = n1 - n2;
+                    Console.WriteLine("O resultado é " + sub);
+                    break;
+                case "/":
+                    double div = n1 / n2;
+                    Console.WriteLine("O resultado é " + div);
+                    break;
+            }
+        }
+    }
+}
